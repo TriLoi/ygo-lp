7ce2d8b3eecbfb9f4be2fd9e73be5b67003b1fa1
TriLoi <triloi.cao@gmail.com>
2018-05-06 11:59:14 +0200

    Model remplacement

--- main.cpp
+++ main.cpp
@@ -17,19 +17,19 @@ int main(int argc, char *argv[])
         return -1;
 
     QObject *v = engine.rootObjects().at(0);
-    Model *m = new Model();
+    Model m;
 
-    QObject::connect(m, SIGNAL(update(int,int)), v, SLOT(update(int, int)));
-    QObject::connect(v, SIGNAL(removeToPlayer1(int)), m, SLOT(removeToPlayer1(int)));
-    QObject::connect(v, SIGNAL(removeToPlayer2(int)), m, SLOT(removeToPlayer2(int)));
-    QObject::connect(v, SIGNAL(addToPlayer1(int)), m, SLOT(addToPlayer1(int)));
-    QObject::connect(v, SIGNAL(addToPlayer2(int)), m, SLOT(addToPlayer2(int)));
-    QObject::connect(v, SIGNAL(dividePlayer1(int)), m, SLOT(dividePlayer1(int)));
-    QObject::connect(v, SIGNAL(dividePlayer2(int)), m, SLOT(dividePlayer2(int)));
-    QObject::connect(v, SIGNAL(resetLP()), m, SLOT(resetLP()));
-    QObject::connect(v, SIGNAL(undoBy(int)), m, SLOT(undoBy(int)));
-    QObject::connect(v, SIGNAL(redoBy(int)), m, SLOT(redoBy(int)));
-    m->initialize();
+    QObject::connect(&m, SIGNAL(update(int,int)), v, SLOT(update(int, int)));
+    QObject::connect(v, SIGNAL(removeToPlayer1(int)), &m, SLOT(removeToPlayer1(int)));
+    QObject::connect(v, SIGNAL(removeToPlayer2(int)), &m, SLOT(removeToPlayer2(int)));
+    QObject::connect(v, SIGNAL(addToPlayer1(int)), &m, SLOT(addToPlayer1(int)));
+    QObject::connect(v, SIGNAL(addToPlayer2(int)), &m, SLOT(addToPlayer2(int)));
+    QObject::connect(v, SIGNAL(dividePlayer1(int)), &m, SLOT(dividePlayer1(int)));
+    QObject::connect(v, SIGNAL(dividePlayer2(int)), &m, SLOT(dividePlayer2(int)));
+    QObject::connect(v, SIGNAL(resetLP()), &m, SLOT(resetLP()));
+    QObject::connect(v, SIGNAL(undoBy(int)), &m, SLOT(undoBy(int)));
+    QObject::connect(v, SIGNAL(redoBy(int)), &m, SLOT(redoBy(int)));
+    m.initialize();
 
     return app.exec();
 }
